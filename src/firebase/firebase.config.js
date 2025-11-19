c17f13ec75e5b80c9e24f1c20ab25b5ee66f4b78
Tanvir <tkdarkshadow0@gmail.com>
2025-11-19 15:29:25 +0600

    first commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
+// Initialize Firebase auth
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
